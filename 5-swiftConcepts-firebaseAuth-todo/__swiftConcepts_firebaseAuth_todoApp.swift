1af16962f891d0e2a80fd65ee6aaeed850742269
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-19 10:08:17 +0800

    - Firebase Setup

--- 5-swiftConcepts-firebaseAuth-todo/__swiftConcepts_firebaseAuth_todoApp.swift
+++ 5-swiftConcepts-firebaseAuth-todo/__swiftConcepts_firebaseAuth_todoApp.swift
@@ -5,10 +5,16 @@
 //  Created by Liu Ziyi on 19/8/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct __swiftConcepts_firebaseAuth_todoApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
