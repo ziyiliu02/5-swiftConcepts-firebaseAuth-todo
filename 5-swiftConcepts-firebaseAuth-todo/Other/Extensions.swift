a3dc3973d73a5268cdc795cd562a393420561f10
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-19 23:20:10 +0800

    - Create Users

---
+++ 5-swiftConcepts-firebaseAuth-todo/Other/Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions.swift
+//  5-swiftConcepts-firebaseAuth-todo
+//
+//  Created by Liu Ziyi on 19/8/23.
+//
+
+import Foundation
+
+extension Encodable {
+    func asDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self) else {
+            return [:]
+        }
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+            return json ?? [:]
+        } catch {
+            return [:]
+        }
+    }
+    
+}
--- 5-swiftConcepts-firebaseAuth-todo/Views/RegisterView.swift
+++ 5-swiftConcepts-firebaseAuth-todo/Views/RegisterView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct RegisterView: View {
     
-    @State var name = ""
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = RegisterViewViewModel()
     
     var body: some View {
         VStack {
@@ -22,20 +20,21 @@ struct RegisterView: View {
                        background: .blue)
             
             Form {
-                TextField("Full Name", text: $name)
+                TextField("Full Name", text: $viewModel.name)
                     .textFieldStyle(DefaultTextFieldStyle())
                     .autocorrectionDisabled()
                 
-                TextField("Email Address", text: $email)
+                TextField("Email Address", text: $viewModel.email)
                     .textFieldStyle(DefaultTextFieldStyle())
                     .autocorrectionDisabled()
                     .autocapitalization(.none)
                 
-                SecureField("Password", text: $password)
+                SecureField("Password", text: $viewModel.password)
                     .textFieldStyle(DefaultTextFieldStyle())
                 
                 TLButton(title: "Create Account", background: .gray) {
                     // Attempt registration
+                    viewModel.register()
                 }
                 .padding()
             }
