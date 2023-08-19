d326b829b825099edadad3191807c2fe3a1c5b47
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-19 09:16:16 +0800

    Initial Commit

---
+++ 5-swiftConcepts-firebaseAuth-todo/__swiftConcepts_firebaseAuth_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftConcepts_firebaseAuth_todoApp.swift
+//  5-swiftConcepts-firebaseAuth-todo
+//
+//  Created by Liu Ziyi on 19/8/23.
+//
+
+import SwiftUI
+
+@main
+struct __swiftConcepts_firebaseAuth_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
