d877e747ba88577ae9bdfe40dac4a0e465037df7
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-20 16:19:53 +0800

    - Save To Do Items

--- 5-swiftConcepts-firebaseAuth-todo/ViewModels/NewItemViewViewModel.swift
+++ 5-swiftConcepts-firebaseAuth-todo/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Liu Ziyi on 19/8/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -16,7 +18,31 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        // Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        // Save model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
