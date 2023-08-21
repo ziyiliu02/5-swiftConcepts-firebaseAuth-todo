60fd54f8cc31aee860c204af0935285b818dea2c
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-21 10:07:15 +0800

    - Done Checkmarks

--- 5-swiftConcepts-firebaseAuth-todo/ViewModels/ToDoListItemViewViewModel.swift
+++ 5-swiftConcepts-firebaseAuth-todo/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,12 +5,26 @@
 //  Created by Liu Ziyi on 19/8/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
